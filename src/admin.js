be07ceb5966191dafc82814e8fae065213b0f267
shunshun <lwwtotod@gmail.com>
2019-04-03 21:48:36 +0800

    init

---
+++ src/admin.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Row, Col } from 'antd'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import NavLeft from './components/NavLeft'
+import './style/common.less'
+
+export default class admin extends Component {
+  render() {
+    return (
+      <Row className="container">
+        <Col span={3} className="nav-left">
+          <NavLeft />
+        </Col>
+        <Col span={21} className="main">
+          <Header>Header</Header>
+          <Row className="content">
+            content
+            {/* {this.props.children} */}
+          </Row>
+          <Footer>Footer</Footer>
+        </Col>
+      </Row>
+    )
+  }
+}
